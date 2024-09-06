edd6beca8fffccd66de6f6c4fce55f77031d2a62
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-06 09:32:08 -0500

    Refactorizar estilos y rutas de fuentes en layout.js y AuthLayout

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -1,10 +1,25 @@
+'use client'
+
+import Link from 'next/link'
 import style from './auth-layout.module.scss'
+import { Icon, Image } from 'semantic-ui-react'
 
 const AuthLayout = ({ children }) => {
   return (
     <section className={style.container}>
-      Auth Layout
-      {children}
+      <div className={style.toBar}>
+        <Link href='/'>
+          <Image src='/logo.png' alt='logo gaming' />
+        </Link>
+
+        <Link href='/'>
+          <Icon name='close' />
+        </Link>
+      </div>
+
+      <div className={style.blockLeft}>{children}</div>
+
+      <div className={style.blockRight} />
     </section>
   )
 }
--- app/auth/sign-up/page.js
+++ app/auth/sign-up/page.js
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import style from './sign-up.module.scss'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className={style.signUp}>
+      <h3>Crear cuenta</h3>
+
+      {/* FORM */}
+
+      <div className={style.actions}>
+        <Link href='/auth/sign-in'>Atras</Link>
+      </div>
+    </div>
   )
 }
 
