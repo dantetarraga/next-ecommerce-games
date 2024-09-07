29a3fad76028c49c4704eb28778dec6284fcdbad
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-06 19:15:59 -0500

    Refactorizar esquema de validación en RegisterForm.js

--- app/auth/components/register-form/RegisterForm.js
+++ app/auth/components/register-form/RegisterForm.js
@@ -1,15 +1,20 @@
 'use client'
 
 import { Form, FormButton, FormGroup, FormInput } from 'semantic-ui-react'
-import { initialValues, validationSchema } from '../../lib'
 import { useFormik } from 'formik'
+import registerSchema from '../../schemas/registerSchema'
 
 const RegisterForm = () => {
   const formik = useFormik({
-    initialValues: initialValues(),
-    validationSchema: validationSchema(),
+    initialValues: {
+      email: '',
+      username: '',
+      name: '',
+      password: ''
+    },
+    validationSchema: registerSchema,
     validateOnChange: false,
-    onSubmit: (values) => {
+    onSubmit: values => {
       console.log(values)
     }
   })
@@ -23,6 +28,8 @@ const RegisterForm = () => {
           type='text'
           placeholder='Correo electronico'
           value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
         />
         <FormInput
           fluid
@@ -30,6 +37,8 @@ const RegisterForm = () => {
           type='text'
           placeholder='Nombre de usuario'
           value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.errors.username}
         />
       </FormGroup>
 
@@ -40,6 +49,8 @@ const RegisterForm = () => {
           type='text'
           placeholder='Nombre y apellidos'
           value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.errors.name}
         />
         <FormInput
           fluid
@@ -47,10 +58,12 @@ const RegisterForm = () => {
           type='password'
           placeholder='Contraseña'
           value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
         />
       </FormGroup>
 
-      <FormButton fluid primary type='submit'>
+      <FormButton fluid primary type='submit' loading={formik.isSubmitting}>
         Registrarse
       </FormButton>
     </Form>
--- app/auth/schemas/registerSchema.js
+++ app/auth/schemas/registerSchema.js
@@ -1,10 +1,10 @@
 import { object, string } from 'yup'
 
 const registerSchema = object().shape({
-  email: string().email().required(),
-  username: string().required(),
-  name: string().required(),
-  password: string().required()
+  email: string().email('Correo electronico invalido.').required('Correo electronico requerido.'),
+  username: string().required('Nombre de usuario requerido.'),
+  name: string().required('Nombre y apellidos requeridos.'),
+  password: string().required('Contraseña requerida.')
 })
 
 export default registerSchema
